c49fca1cd5707a4203bf7eb2ca613b9eae0ad14d
mgkraynova <Marina.Kraynova@leroymerlin.ru>
2024-01-07 23:15:56 +0300

    add example redux toolkit logic

---
+++ src/features/counter/counter.tsx
@@ -0,0 +1,27 @@
+import { decrement, increment } from "./counter-slice";
+import { useAppDispatch, useAppSelector } from "../../hooks";
+
+export function Counter() {
+  const count = useAppSelector((state) => state.counterReducer.value);
+  const dispatch = useAppDispatch();
+
+  return (
+    <div>
+      <div>
+        <button
+          aria-label="Increment value"
+          onClick={() => dispatch(increment())}
+        >
+          Increment
+        </button>
+        <span>{count}</span>
+        <button
+          aria-label="Decrement value"
+          onClick={() => dispatch(decrement())}
+        >
+          Decrement
+        </button>
+      </div>
+    </div>
+  );
+}
